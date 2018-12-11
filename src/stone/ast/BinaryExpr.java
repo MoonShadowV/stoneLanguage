e08ce50a53a09bf6a1f5179a0df3d4ee1aae1bcd
ShadowMoonV <834540085@qq.com>
2018-12-11 22:51:25 +0800

    feat: basic ASTree

---
+++ src/stone/ast/BinaryExpr.java
@@ -0,0 +1,20 @@
+package stone.ast;
+
+import java.util.List;
+
+public class BinaryExpr extends ASTList {
+    public BinaryExpr(List<ASTree> children) {
+        super(children);
+    }
+    public ASTree left(){
+        return child(0);
+    }
+
+    public ASTree right(){
+        return child(2);
+    }
+
+    public String operator(){
+        return ((ASTLeaf)child(1)).getToken().getText();
+    }
+}
---
+++ src/stone/ast/NumberLiteral.java
@@ -0,0 +1,12 @@
+package stone.ast;
+
+import stone.Token;
+
+public class NumberLiteral extends ASTLeaf {
+    public NumberLiteral(Token token) {
+        super(token);
+    }
+    public int value(){
+        return getToken().getNumber();
+    }
+}
